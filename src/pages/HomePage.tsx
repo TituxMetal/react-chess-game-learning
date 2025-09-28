0eb760cac6f6fca132beb5a4159894715bbed63e
Titux Metal <github@lgdweb.fr>
2025-09-28 14:26:20 +0200

    Fix SVG data URL syntax error

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -12,7 +12,6 @@ const HomePage = () => {
     <div className="min-h-screen bg-gradient-to-br from-slate-950 via-slate-900 to-slate-800 relative overflow-hidden">
       {/* Background decoration */}
       <div className="absolute inset-0 bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-blue-900/20 via-slate-900/50 to-slate-950"></div>
-      <div className="absolute top-0 left-0 w-full h-full bg-[url('data:image/svg+xml,%3Csvg width="60" height="60" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg"%3E%3Cg fill="none" fill-rule="evenodd"%3E%3Cg fill="%23ffffff" fill-opacity="0.02"%3E%3Ccircle cx="30" cy="30" r="2"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')]"></div>
       
       <div className="relative z-10 min-h-screen flex flex-col">
         {/* Header */}
